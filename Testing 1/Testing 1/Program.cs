cc54cafe396bda1cbd9315e84b1beb1823ab4c07
ViggoKlo <viggo.kloberg@gmail.com>
2022-08-29 12:32:56 +0200

    commit

---
+++ Testing 1/Testing 1/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+var b = 1;
+
+while (b <= 9)
+{
+    var a = 0;
+
+    while (a <= 9)
+    {
+        a++;
+
+        Console.Write(a * b);
+
+        Console.Write(" ");
+    }
+
+    b++;
+
+    Console.WriteLine("");
+}
+
+
+
+
+
